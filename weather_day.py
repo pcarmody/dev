05688118b416824acd9c46007af178f9b83e160a
Paul Carmody <pcarmody05@gmail.com>
2018-04-29 10:55:46 -0700

    updated weather_day

--- weather_day.py
+++ weather_day.py
@@ -58,6 +58,55 @@ def convert_time_to_minutes(hours, minutes, am):
       hours = hours + 12
   return hours * 60 + minutes
 
+def get_white_color(center, scope, trans):
+  half = (scope[1] - scope[0] ) / 2
+  first = (scope[1] - scope[0] ) / 4
+  third = 3 * (scope[1] - scope[0] ) / 4
+  red = 0
+  blue = 0
+  green = 0
+  if(center < half):
+    red = 255 * (first - abs(first - center))/first
+  if(center > half):
+    blue = 255 * (first - abs(third - center))/first
+  if(center >= first and center < third):
+    green = 255 * (first - abs(half - center))/first
+  else:
+    if center < first:
+      green = 255 * (first - center)/first
+      blue = 255 * (first - center)/first
+      red = 255
+    else:
+      green = 255 * abs(third - center)/first
+      red = 255 * abs(third - center)/first
+      blue = 255
+  return (red, green, blue, trans)
+
+def get_black_color(center, scope):
+  half = (scope[1] - scope[0] ) / 2
+  first = (scope[1] - scope[0] ) / 4
+  third = 3 * (scope[1] - scope[0] ) / 4
+  if(center < half):
+    red = 255 * (first - abs(first - center))/first
+  else:
+    red = 0
+  if(center > first and center <= third):
+    green = 255 * (first - abs(half - center))/first
+  else:
+    green = 0
+  if(center < half):
+    blue = 0
+  else:
+    blue = 255 * (first - abs(third - center))/first
+  color = (red, green, blue, 255)
+  return color
+
+def get_color(center, scope):
+  half = (scope[1] - scope[0] ) / 2
+  if center > half:
+    return get_white_color(center, scope, 255)
+  return get_black_color(center, scope)
+
 def draw_halves(size, temp_rise, temp_set, day_color):
   image = Image.new('RGBA', (size, size), (0,0,0,0))
   draw = ImageDraw.Draw(image)
@@ -103,20 +152,115 @@ def add_lines(beg, end):
 def draw_temp_ring(shape):
   image = Image.new('RGBA',(window_size,window_size),(0,0,0,0))
   draw = ImageDraw.Draw(image)
+  num_wedges = 100
+  delta_hour = num_wedges / 24
+  wedge = 36000 / num_wedges
+  for j in range(0,num_wedges):
+    first = ((j*100)/num_wedges) * 24/100
+    if(first == 23):
+      second = 0
+    else:
+      second = first + 1
+    delta_B = j % delta_hour
+    delta_A = delta_hour - delta_B
+    beg = wedge * j / 100
+    end = (j+1) * wedge / 100
+    temp1 = int(weather[first]['Teperature'][0]) * 10 + int(weather[first]['Teperature'][1])
+    temp2 = int(weather[second]['Teperature'][0]) * 10 + int(weather[second]['Teperature'][1])
+    temp = (temp1*delta_A + temp2*delta_B) / delta_hour
+    print str(j)+':'+str(first)+':'+str(temp)+':'+str(beg)+':'+str(end)
+    color = get_color(110-temp, (20, 110)) 
+    draw.pieslice(shape, beg, end, color, color)
+  return image
+
+def draw_wind_ring(shape):
+  image = Image.new('RGBA',(shape,shape),(0,0,0,0))
+  draw = ImageDraw.Draw(image)
   wedge = 360 / 24
-  for i in range(0,20):
+  for i in range(0,24):
     beg = wedge * i
     end = beg + wedge
-    temp = int(weather[i]['Teperature'][0]) * 10 + int(weather[i]['Teperature'][1]) - 40 
-    color = (0,0,255, 255 * temp / 20)
-    draw.pieslice(shape, beg, end, color, color)
+    wind = weather[i]['Wind'].split(' ')
+    dirstr = wind[0]
+    if(dirstr == 'Calm'):
+      color = (0, 0, 0, 255)
+      speed = 0
+    else:
+      direction = 0
+      tmp = 0
+      for j in ['N', 'NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE', 'S', 'SSW', 'SW', 'WSW', 'W', 'WNW', 'NW', 'NNW' ]:
+        if(j == dirstr):
+          direction = tmp
+        tmp = tmp + 1
+      speed = int(wind[1])
+      color = get_white_color(direction, (0,15), 255)
+
+    draw.pieslice((0,0,shape,shape), beg, end, color, color)
+  return image
+
+def wind_speed(shape):
+  image = Image.new('RGBA',(shape,shape),(0,0,0,0))
+  draw = ImageDraw.Draw(image)
+  wedge = 360 / 24
+  for i in range(0,24):
+    beg = wedge * i
+    end = beg + wedge
+    wind = weather[i]['Wind'].split(' ')
+    dirstr = wind[0]
+    if(dirstr == 'Calm'):
+      color = (0, 0, 0, 255)
+      speed = 0
+    else:
+      speed = int(wind[1])
+    max_speed = 15
+    speed_color = (0,0,0,255*(max_speed - speed)/max_speed)
+    draw.pieslice((0,0,shape,shape), beg, end, speed_color, speed_color)
+  return image
+
+def precip_ring(shape):
+  image = Image.new('RGBA',(shape,shape),(0,0,0,0))
+  draw = ImageDraw.Draw(image)
+  wedge = 360 / 24
+  for i in range(0,24):
+    beg = wedge * i
+    end = beg + wedge
+    precip = int(weather[i]['Precipitation'].split('%')[0])
+    color = get_color(precip, (0,50)) #(0,0,0,255*speed/8)
+    draw.pieslice((0,0,shape,shape), beg, end, color, color)
+  return image
+
+def humidity_ring(shape):
+  image = Image.new('RGBA',(shape,shape),(0,0,0,0))
+  draw = ImageDraw.Draw(image)
+  wedge = 360 / 24
+  for i in range(0,24):
+    beg = wedge * i
+    end = beg + wedge
+    precip = int(weather[i]['Humidity'].split('%')[0])
+    color = get_color(precip, (0,100)) #(0,0,0,255*speed/8)
+    draw.pieslice((0,0,shape,shape), beg, end, color, color)
   return image
 
 #temp_ring = draw.ellipse(ring_shape(0),'blue','blue')
 img = draw_temp_ring(ring_shape(0))
 image.paste(img, ring_shape(0), img)
-wind_ring = draw.ellipse(ring_shape(1),'red','blue')
-precip_tide = draw.ellipse(ring_shape(2),'black','blue')
+
+#wind_ring = draw.ellipse(ring_shape(1),'red','blue')
+img = draw_wind_ring(ring_size(1))
+image.paste(img, ring_shape(1), img)
+
+#wind_ring = draw.ellipse(ring_shape(1),'red','blue')
+img = wind_speed(ring_size(1))
+image.paste(img, ring_shape(1), img)
+
+#precip_tide = draw.ellipse(ring_shape(2),'yellow','blue')
+img = precip_ring(ring_size(2))
+image.paste(img, ring_shape(2), img)
+
+img = humidity_ring(ring_size(3))
+image.paste(img, ring_shape(3), img)
+
+precip_tide = draw.ellipse(ring_shape(4),'black','blue')
 
 #draw hourly lines
 for beg in [0, 60, 120, 180, 240, 300]:
--- wedge_color.py
+++ wedge_color.py
@@ -11,6 +11,55 @@ draw = ImageDraw.Draw(image)
 
 black = (0, 0, 0, 0)
 
+def get_white_color(center, scope):
+  half = (scope[1] - scope[0] ) / 2
+  first = (scope[1] - scope[0] ) / 4
+  third = 3 * (scope[1] - scope[0] ) / 4
+  red = 0
+  blue = 0
+  green = 0
+  if(center < half):
+    red = 255 * (first - abs(first - center))/first
+  if(center > half):
+    blue = 255 * (first - abs(third - center))/first
+  if(center >= first and center < third):
+    green = 255 * (first - abs(half - center))/first
+  else:
+    if center < first:
+      green = 255 * (first - center)/first
+      blue = 255 * (first - center)/first
+      red = 255
+    else:
+      green = 255 * abs(third - center)/first
+      red = 255 * abs(third - center)/first
+      blue = 255
+  return (red, green, blue, 255)
+
+def get_black_color(center, scope):
+  half = (scope[1] - scope[0] ) / 2
+  first = (scope[1] - scope[0] ) / 4
+  third = 3 * (scope[1] - scope[0] ) / 4
+  if(center < half):
+    red = 255 * (first - abs(first - center))/first
+  else:
+    red = 0
+  if(center > first and center <= third):
+    green = 255 * (first - abs(half - center))/first
+  else:
+    green = 0
+  if(center < half):
+    blue = 0
+  else:
+    blue = 255 * (first - abs(third - center))/first
+  color = (red, green, blue, 255)
+  return color
+
+def get_color(center, scope):
+  half = (scope[1] - scope[0] ) / 2
+  if center > half:
+    return get_white_color(center, scope)
+  return get_black_color(center, scope)
+
 def wedges(count):
   accuracy = 1000
   delta = 360 * accuracy / (count+1)
@@ -19,28 +68,12 @@ def wedges(count):
   for i in range(0,count+1):
           #    beg = i * delta / 100
     end = (i+1) * delta / accuracy 
-#    center = (beg + end) / 2
-    center = beg 
-    if(center < 180):
-      red = 255 * (90 - abs(90 - center))/90
-    else:
-      red = 0
-    if(center > 90 and center < 270):
-      green = 255 * (90 - abs(180 - center))/90
-    else:
-      green = 0
-    if(center < 179):
-      blue = 0
-    else:
-      blue = 255 * (90 - abs(270 - center))/90
-    color = (red, green, blue, 255)
-    print i
-    print beg
-    print color
+    #color = get_color(beg, (0, 360), count+1)
+    color = get_color(beg, (0, 360))
     draw.pieslice((0,0,size, size), beg, end, color, color)
     beg = end
   return
 
-wedges(15)
+wedges(127)
 
 image.rotate(90).show()
